fc9ed8c6e477b8bc77b1391b4d5ce9a55535f99a
shema kaitakirwa <skaitakirwa@gmail.com>
2025-07-01 00:25:46 +0200

    Implement components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import family from './family.svg';
+import React, { FC } from 'react';
+import Timeline from './components/Timeline';
+import moments from './common/moments.json';
+import { Moment } from './common/types';
+import './App.scss';
 
-function App() {
+const App: FC = () => {
   return (
     <main>
-     <div 
-        style={{background: "#173F4B", width: "130px", height: "136px", boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.25)"}}>
-          <img src={family} alt="Family" />
-          <p style={{textAlign: "center"}}>Baby's birth</p>
-     </div>
+      <Timeline
+        moments={moments as Moment[]}
+        referenceDate='2024-01-01' // Example reference date
+      />
     </main>
   );
 }
